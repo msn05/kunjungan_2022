b341cc0031e94457c3026384d57e0da32aff27e6
satrio05 <m.satrion05091997@gmail.com>
2022-07-17 21:21:06 +0700

    change name app and add const app init

--- app/init.php
+++ app/init.php
@@ -4,6 +4,7 @@ error_reporting(0);
 if (!session_id()) session_start();
 require_once __DIR__ . '/../vendor/autoload.php';
 // require_once __DIR__ . '/../app/init.php';
+define('App', 'Aplikasi Kunjungan Inspektorat provinsi Sumatera Selatan');
 define('Controller', 'http://localhost/kunjungan_inspektorat_prov/');
 define('DOC_ROOT', 'http://localhost/kunjungan_inspektorat_prov/public');
 define('Vendor', 'http://localhost/kunjungan_inspektorat_prov/vendor/');
--- app/views/template/header.php
+++ app/views/template/header.php
@@ -12,7 +12,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>AdminLTE 3 | Starter</title>
+  <title><?= App; ?></title>
 
   <!-- Google Font: Source Sans Pro -->
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
@@ -169,7 +169,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
                   <div class="form-group row">
                     <label for="inputEmail3" class="col-sm-5 col-form-label">Nama</label>
                     <div class="col-sm-7">
-                      <input type="text" class="form-control" name="name_employee" id="inputEmail3" placeholder="Nama Pegawai Pengajuan Kunjungan">
+                      <input type="text" class="form-control" name="name_employee" id="inputEmail3" placeholder="Nama Pengajuan Kunjungan">
                     </div>
                   </div>
                   <div class="form-group row">
